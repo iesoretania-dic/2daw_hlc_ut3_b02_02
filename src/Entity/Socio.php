b81e366620b2977c9b0c9f74d80035a1c8b5460f
YoussefAFdez <youssef.a.fdez@gmail.com>
2022-01-31 00:43:34 +0100

    Agregado Constructor para coleccion vacia

--- src/Entity/Socio.php
+++ src/Entity/Socio.php
@@ -2,6 +2,7 @@
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
@@ -59,6 +60,14 @@ class Socio
      * @var Libro[]|Collection
      */
     private $libros;
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->libros = new ArrayCollection();
+    }
 
     /**
      * @return int
